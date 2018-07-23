872ff8f1386ed55ed17fe62548d3feb99a435e4f
mihirsam <xenon.of.x@gmail.com>
2018-07-23 20:02:53 +0530

    complete word cloud assignment

--- Week2/word_cloud/mine/word_cloud.py
+++ Week2/word_cloud/mine/word_cloud.py
@@ -1,7 +1,34 @@
-file = open("98-0.txt", "r")
-stopwords = open("stopwords.txt", "r")
+file = open("98-0.txt","r")
+stopword = open("stopwords.txt", "r")
+
+stopwords = []
+for word in stopword:
+    stopwords.append(word.strip("\n"))
 
 word_cloud = {}
-
 for word in file:
-    print(word)
+    words = []
+    words = word.split(' ')
+
+    for i in range(0, len(words)):
+        if words[i] not in word_cloud.keys():
+            if words[i] not in stopwords:
+                word_cloud[words[i]] = 1
+            else:
+                pass
+        else:
+            word_cloud[words[i]] += 1
+
+
+result = sorted(word_cloud.items(), key=lambda x: x[1], reverse = True)
+
+print("Word_Cloud: \n")
+
+i = 0
+for key, value in result:
+    print(key, ":", value)
+    i += 1
+    if i > 10:
+        break
+    else:
+        pass
